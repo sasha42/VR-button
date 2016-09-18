594b5d1b16e2473a55b66778846ae696171f8160
sasha42 <sasha42@users.noreply.github.com>
2016-09-18 14:49:57 +0200

    cors stuff and migration

--- button/views.py
+++ button/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Button
 
+from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
+
+@xframe_options_exempt
 def button_preview(request, id):
     button = get_object_or_404(Button, id=id)
     return render(request, 'button/button.html', {'button': button})
+    #return HttpResponse("This page is safe to load in a frame on any site.")
